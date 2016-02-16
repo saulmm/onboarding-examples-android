5d7c359947ddcc95db639774d97dea488f936477
Saul M <saulmm2@gmail.com>
2016-02-16 16:42:09 +0100

    Added simple case

---
+++ app/src/main/java/com/example/saulmm/splashes/OnBoardingWithSimpleBackground.java
@@ -0,0 +1,28 @@
+package com.example.saulmm.splashes;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by saulmm on 16/02/16.
+ */
+public class OnBoardingWithSimpleBackground extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Fake a long startup time
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_onboarding_simple);
+            }
+
+        }, 500);
+
+
+    }
+}
