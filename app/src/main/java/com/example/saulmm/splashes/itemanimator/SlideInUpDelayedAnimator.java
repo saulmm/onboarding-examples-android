b2f2143cdd809e29fc589f6ba462bbdba5dfbfb2
Saul Molinero <saulmm2@gmail.com>
2017-02-24 17:16:55 +0100

    Added license

--- app/src/main/java/com/example/saulmm/splashes/itemanimator/SlideInUpDelayedAnimator.java
+++ app/src/main/java/com/example/saulmm/splashes/itemanimator/SlideInUpDelayedAnimator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.saulmm.splashes.itemanimator;
 
 import android.support.v4.view.ViewCompat;
@@ -6,12 +21,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.animation.Interpolator;
 
 
-public class SlideInUpDelayedAnimator extends BaseItemAnimator {
+class SlideInUpDelayedAnimator extends BaseItemAnimator {
 
-    private final int offsetDelay = 200;
     private final Interpolator mInterpolator;
 
-    public SlideInUpDelayedAnimator(Interpolator interpolator) {
+    SlideInUpDelayedAnimator(Interpolator interpolator) {
         mInterpolator = interpolator;
     }
 
@@ -20,12 +34,13 @@ public class SlideInUpDelayedAnimator extends BaseItemAnimator {
         ViewCompat.setTranslationY(holder.itemView, holder.itemView.getHeight());
         ViewCompat.setAlpha(holder.itemView, 0);
     }
-    
+
     @Override
     protected ViewPropertyAnimatorCompat onAnimatedAdd(RecyclerView.ViewHolder holder) {
+        int offsetDelay = 50;
         return ViewCompat.animate(holder.itemView)
                 .translationY(0)
                 .setInterpolator(mInterpolator)
-                .setStartDelay(offsetDelay*holder.getLayoutPosition());
+                .setStartDelay(offsetDelay *holder.getLayoutPosition());
     }
 }
